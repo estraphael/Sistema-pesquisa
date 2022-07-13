0222688bec534f9f5f4e422269282629e6dd8461
Esteferson Raphael <91302139+estraphael@users.noreply.github.com>
2022-07-13 09:10:30 -0300

    finalizei a versão beta e retirei os alerts

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-alert("Olá, você! Eu sei que esse tipo de pop up é chato, mas é importante e bem rápido, prometo. Você só vai precisar clicar em 'ok' mais quatro vezes. Muito em breve eu vou retirar essa chatice aqui :)")
-alert("1- Eu sou o Esteferson. Programação não é minha área, mas é algo que eu sempre quis aprender e recentemente decidi estudar essa maravilha.")
-alert("2- Como sou iniciante em programação, é muito importante para mim que qualquer bug seja informado. Caso note qualquer mau funcionamento, por favor, me informe.")
-alert("3- Por último, uma pequena instrução: para fazer a pesquisa, atualmente é necessário utilizar maiúsculas e minúsculas corretamente, senão a ferramenta não filtrará os resultados.")
-alert("4- Bem, é isso. Espero que a ferramenta seja útil!")
-
 for(i of document.querySelectorAll('[search]')){
     try{
         busca(i,document.querySelector("#"+i.getAttribute('search')))
